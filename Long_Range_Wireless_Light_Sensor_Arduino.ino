8e373e351ce70a5115a6380982271a13d898b3d4
Bhaskar <bhaskar.anil429@gmail.com>
2018-04-17 16:32:28 -0500

    Add files via upload

---
+++ Long_Range_Wireless_Light_Sensor_Arduino.ino
@@ -0,0 +1,70 @@
+///this code is written and tested for ncd.io IoT Long Range Wireless Light with arduino due
+///sensor data structure can be found here https://ncd.io/long-range-iot-wireless-light-sensor-product-manual/
+/// sensor can be found here https://store.ncd.io/product/iot-long-range-wireless-light-sensor/
+
+  uint8_t data[29];
+  int k = 10;
+  int i;
+void setup()
+{
+  Serial1.begin(9600);
+  Serial.begin(9600);
+  Serial.println("ncd.io IoT Arduino Wireless Light Sensor");
+}
+
+void loop()
+{
+  
+  if (Serial1.available())
+  {
+    data[0] = Serial1.read();
+    delay(k);
+   if(data[0]==0x7E)
+    {
+    while (!Serial1.available());
+    for ( i = 1; i< 29; i++)
+      {
+      data[i] = Serial1.read();
+      delay(1);
+      }
+    if(data[15]==0x7F)  /////// to check if the recive data is correct
+      {
+  if(data[22]==0x0A)  //////// make sure the sensor type is correct
+        {  
+          long int lux = ((data[24]<<8) + (data[25])) ;
+   float battery = ((data[18] * 256) + data[19]);
+  float voltage = 0.00322 * battery;
+  Serial.print("Sensor Number  ");
+  Serial.println(data[16]);
+  Serial.print("Sensor Type  ");
+  Serial.println(data[22]);
+  Serial.print("Firmware Version  ");
+  Serial.println(data[17]);
+  Serial.print("Light Intensity is :");
+  Serial.println(lux);
+  Serial.print(" Lux");
+  Serial.print("ADC value:");
+  Serial.println(battery);
+  Serial.print("Battery Voltage:");
+  Serial.print(voltage);
+  Serial.println("\n");
+  if (voltage < 1)
+          {
+    Serial.println("Time to Replace The Battery");
+          }
+  
+        }
+      }
+else
+{
+      for ( i = 0; i< 29; i++)
+    {
+      Serial.print(data[i]);
+      Serial.print(" , ");
+      delay(1);
+    }
+}
+    }
+  }
+ 
+}
